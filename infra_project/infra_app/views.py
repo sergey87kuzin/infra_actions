de4b0b66a68b68a6417b3b124728534832c52eb1
sergey87kuzin <sergeykuzin@sergeys-MacBook-Air.local>
2021-09-02 09:41:04 +0300

    docker_

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -6,4 +6,4 @@ def index(request):
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница! Зачем она тут?')
+    return HttpResponse('А это вторая страница! Зачем она тут? ')
